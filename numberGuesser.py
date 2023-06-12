631154ae2c37ea7b3da42194811f1ddb4952811b
Jacob Ross <ekroththegreat@Jacobs-Air.lan>
2023-06-11 21:58:11 -0500

    first commmit

--- numberGuesser.py
+++ numberGuesser.py
@@ -1,4 +1,13 @@
 
+def generate_car_image():
+    car_image = r'''
+           _______
+  _||____||___\_____
+ (   '#cars' ____  #\
+  '._____.'`______,'_)
+
+'''
+    print(car_image)
 
 
 
@@ -6,6 +15,12 @@
 
 name = str(input("What is your name?"))
 print(f"Hello {name}")
+location = str(input("Where to ? Gym Or Commissary"))
+if location == str("gym") :
+    print("Lets go to the gym! ")
 
+if location == str("commissary"):
+    print("Lets go to the Commissary 🥦 ")
 
+print(f"Brr brr Im in me moms car {generate_car_image()}")
 
