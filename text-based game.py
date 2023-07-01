c87978a9350cc78e89560867ea14f9827c75ef6f
Jacob Ross <ekroththegreat@Jacobs-MacBook-Air.local>
2023-07-01 08:58:46 -0500

    using the max and min methods for the first tie

--- text-based game.py
+++ text-based game.py
@@ -1,60 +1,90 @@
 
-# Treasure island
-print('''*******************************************************************************
-          |                   |                  |                     |
- _________|________________.=""_;=.______________|_____________________|_______
-|                   |  ,-"_,=""     `"=.|                  |
-|___________________|__"=._o`"-._        `"=.______________|___________________
-          |                `"=._o`"=._      _`"=._                     |
- _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
-|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
-|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
-          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
- _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
-|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
-|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
-____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
-/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
-____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
-/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
-____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
-/______/______/______/______/______/______/______/______/______/______/[TomekK]
-*******************************************************************************
-''')
+# # Treasure island
+# print('''*******************************************************************************
+#           |                   |                  |                     |
+#  _________|________________.=""_;=.______________|_____________________|_______
+# |                   |  ,-"_,=""     `"=.|                  |
+# |___________________|__"=._o`"-._        `"=.______________|___________________
+#           |                `"=._o`"=._      _`"=._                     |
+#  _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+# |                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+# |___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+#           |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+#  _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+# |                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+# |___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+# ____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+# /______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+# ____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+# /______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+# ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+# /______/______/______/______/______/______/______/______/______/______/[TomekK]
+# *******************************************************************************
+# ''')
 
-name = input("What is your Name?")
-print (f"Welcome {name} to Treasure Island.")
-first_Q = input(f"{name} which way do you want to start this journey , straight right or left").lower()
-
-
-if first_Q == "straight" :
-    second_q =input(str("Right choice, now you see a bridge choose straight or right or left")).lower()
-    if second_q == "left" :
-        print(f"{name} you made it to the treasure")
-
-    else: (f"{name} you died from babboons . Game Over")
+# name = input("What is your Name?")
+# print (f"Welcome {name} to Treasure Island.")
+# first_Q = input(f"{name} which way do you want to start this journey , straight right or left").lower()
 
 
+# if first_Q == "straight" :
+#     second_q =input(str("Right choice, now you see a bridge choose straight or right or left")).lower()
+#     if second_q == "left" :
+#         print(f"{name} you made it to the treasure")
+
+#     else: (f"{name} you died from babboons . Game Over")
 
 
 
 
 
-elif first_Q == "right" :
-  second_q =input(str("Right choice, now you see a somthing that was recently buried, do you wanna investagate yes or no ")).lower()
-  if second_q == "yes" :
-    print(f"{name} you died, it was a claymore!! ")
 
-  elif second_q == "no": (f"{name} you now see a lake the left side is blocked off so you can swim accross or walk around the right side , which one swim of right")
+
+# elif first_Q == "right" :
+#   second_q =input(str("Right choice, now you see a somthing that was recently buried, do you wanna investagate yes or no ")).lower()
+#   if second_q == "yes" :
+#     print(f"{name} you died, it was a claymore!! ")
+
+#   elif second_q == "no": (f"{name} you now see a lake the left side is blocked off so you can swim accross or walk around the right side , which one swim of right")
   
-  third_Q = input(f"{name} which way do you want to start this journey , straight right or left").lower()
-  if third_Q == "swim" :
-    print(f"{name} you were eaten. Game Over")
-  elif third_Q == "right" :
-   print(f"{name} you made it to the treasure")
+#   third_Q = input(f"{name} which way do you want to start this journey , straight right or left").lower()
+#   if third_Q == "swim" :
+#     print(f"{name} you were eaten. Game Over")
+#   elif third_Q == "right" :
+#    print(f"{name} you made it to the treasure")
 
 
 
-elif first_Q == "left" :
-   print(f"{name} you were eaten. Game Over")
+# elif first_Q == "left" :
+#    print(f"{name} you were eaten. Game Over")
+
+
+
+
+
+# Useing the max method you can sort to the highest of each data type, exceppted booleans. 
+class_scores = [98,110,15,1,34,26,33,346,0.1,56,3445,91,234,60,553]# it even sorts floats / deciamals
+array2 = [1, 2, 3, 4, 5]
+array3 = ['apple', 'banana', 'cherry', 'date','zack','paul']
+array5 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+array6 = list(range(1, 1001))
+
+# print(len(class_scores))
+# class_scores.sort()
+# print(class_scores)
+
+# print(len(class_scores))
+# print(class_scores[13])
+# print(max(class_scores))
+# print(max(array2))
+# print(max(array3))
+# print(max(array5))
+# print(max(array6))
+
+# Useing the min method you can sort to the lowest of each data type, exceppted booleans. 
+# print(min(class_scores))
+# print(min(array2))
+# print(min(array3))
+# print(min(array5))
+# print(min(array6))
 
